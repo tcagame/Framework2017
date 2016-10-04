6b73fe00255e3bacf0d85870d654d448cbf0b366
okuty <okuty.phone@gmail.com>
2016-10-04 19:58:41 +0900

    Testプロジェクト修正

--- Test/main.cpp
+++ Test/main.cpp
@@ -1,5 +1,32 @@
 #include "Application.h"
+#include "Drawer.h"
+
+class Test : public Task {
+public:
+	static std::string getTag( ) { return "TEST"; }
+	Test( ) {
+		
+	}
+
+	virtual ~Test( ) {
+
+	}
+
+	void initialize( ) {
+		DrawerPtr drawer = Drawer::getTask( );
+		drawer->loadGraph( "" );
+	}
+
+	void update( ) {
+
+	}
+};
 
 void main( ) {
+	TaskPtr test = TaskPtr( new Test );
+	TaskPtr drawer = TaskPtr( new Drawer( "Resource" ) );
 
+	ApplicationPtr app = Application::getInstance( );
+	app->addTask( Test::getTag( ), test );
+	app->addTask( Drawer::getTag( ), drawer );
 }
